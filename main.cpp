e36aa0f572514a842533bca9c9ee43fe3f28716b
Yamen Allaf <yamen_allaf@rawiplatform.com>
2024-12-14 23:20:34 +0300

    VkInstance creation all setup

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
-const uint32_t WIDTH = 1920;
-const uint32_t HEIGHT = 1080;
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
 
 class HelloTriangleApplication {
 public:
@@ -23,13 +25,13 @@ private:
         glfwInit();
 
         glfwInitHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
     void initVulkan() {
-
+        createInstance();
     }
 
     void mainLoop() {
@@ -41,11 +43,86 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+
+        VkInstanceCreateInfo createInfo {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        
+
+        std::vector<const char*> requiredExtensions;
+
+
+        for(uint32_t i = 0; i < glfwExtensionCount; i++) {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+
+        requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+
+        createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
+        createInfo.enabledExtensionCount = (uint32_t) requiredExtensions.size();
+        createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for(int i = 0; i < extensions.size(); i ++) {
+            std::cout << '\t' << extensions[i].extensionName << '\n';
+        }
+
+        bool allSupportedExtensionsReturnedByGLFW = true;
+
+        if(extensions.size() != requiredExtensions.size()) {
+            allSupportedExtensionsReturnedByGLFW = false;
+            std:: cout << "extensions required does not match extensions supported by vulkan";
+            return;
+        }
+
+
+        for(int i = 0; i < extensions.size(); i ++) {
+            if(extensions[i].extensionName != requiredExtensions[i]) {
+                allSupportedExtensionsReturnedByGLFW = false;
+                break;
+            }
+        }
+    
+        allSupportedExtensionsReturnedByGLFW ? std:: cout << "extensions required match extensions supported by vulkan": std:: cout << "extensions required does not match extensions supported by vulkan"; 
 
     }
 
-
     GLFWwindow* window;
+    VkInstance instance;
 };
 
 int main() {
