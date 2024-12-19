8e0c60b01fa49a7ba76cbd1990d6c9accd8408f5
Yamen Allaf <yamen_allaf@rawiplatform.com>
2024-12-19 19:17:12 +0300

    rating device suitability disabled for now....... check of queue families of physical device is now enabled

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include <optional>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
@@ -127,35 +128,37 @@ private:
         // }
     }
 
-    int rateDeviceSuitability(VkPhysicalDevice device)
-    {
-        VkPhysicalDeviceProperties deviceProperties;
-        VkPhysicalDeviceFeatures deviceFeatures;
-        vkGetPhysicalDeviceProperties(device, &deviceProperties);
-        vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
-        int score = 0;
+    // int rateDeviceSuitability(VkPhysicalDevice device)
+    // {
+    //     VkPhysicalDeviceProperties deviceProperties;
+    //     VkPhysicalDeviceFeatures deviceFeatures;
+    //     vkGetPhysicalDeviceProperties(device, &deviceProperties);
+    //     vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+    //     int score = 0;
 
-        // Discrete GPUs have a significant performance advantage
-        if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
-        {
-            score += 1000;
-        }
+    //     // Discrete GPUs have a significant performance advantage
+    //     if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+    //     {
+    //         score += 1000;
+    //     }
 
-        // Maximum possible size of textures affects graphics quality
-        score += deviceProperties.limits.maxImageDimension2D;
+    //     // Maximum possible size of textures affects graphics quality
+    //     score += deviceProperties.limits.maxImageDimension2D;
 
-        // Application can't function without geometry shaders
-        if (!deviceFeatures.geometryShader)
-        {
-            return 0;
-        }
+    //     // Application can't function without geometry shaders
+    //     if (!deviceFeatures.geometryShader)
+    //     {
+    //         return 0;
+    //     }
 
-        return score;
-    }
+    //     return score;
+    // }
 
     bool isDeviceSuitable(VkPhysicalDevice device)
     {
-        return true;
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+        return indices.isComplete();
     }
 
     int rateDeviceSuitability(VkPhysicalDevice device)
@@ -361,6 +364,45 @@ private:
         {
             throw std::runtime_error("failed to set up debug messenger!");
         }
+    }
+
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete()
+        {
+            return graphicsFamily.has_value();
+        }
+    };
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto &queueFamily : queueFamilies)
+        {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            {
+                indices.graphicsFamily = i;
+            }
+
+            if (indices.isComplete())
+            {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
     }
 
     GLFWwindow *window;
